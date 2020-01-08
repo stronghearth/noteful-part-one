b8db7e85ecde6bbcb76af1ad6ed63416d0d43d7b
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-01-08 12:30:25 -0500

    wired Header, Sidebar, and Main components with filler HTML and basic CSS

---
+++ src/main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import main from './main.css'
+
+function Main () {
+    return <main>
+            <nav className="mainNav">
+                <ul>
+                    <li>note here</li>
+                    <li>note there</li>
+                    <li>note everywhere</li>
+                </ul>
+            </nav>
+    </main>
+}
+
+export default Main
---
+++ src/sidebar/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import sidebar from './sidebar.css'
+
+function Sidebar () {
+    return <nav className="sidebar">
+            <ul>
+                <li>
+                    folder here
+                </li>
+                <li>
+                    folder there
+                </li>
+                <li>
+                    folder everywhere
+                </li>
+            </ul>
+    </nav>
+}
+
+export default Sidebar
